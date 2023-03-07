1ed8044fd400831329fb2997bd571d522c2f66e3
remichu <remichu@example.com>
2023-03-07 23:05:06 +0800

    update

--- vars/create.groovy
+++ vars/create.groovy
@@ -1,10 +1,13 @@
-pipeline {
-    agent { docker { image 'maven:3.9.0-eclipse-temurin-11' } }
-    stages {
-        stage('build') {
-            steps {
-                //test
-                sh 'mvn --version'
+
+def call() {
+    pipeline {
+        agent { docker { image 'maven:3.9.0-eclipse-temurin-11' } }
+        stages {
+            stage('build') {
+                steps {
+                    //test
+                    sh 'mvn --version'
+                }
             }
         }
     }
